32f51ff8cfeaa7bb556d14056664734778d6033d
rishabhrajphx <rishabhnewmob@gmail.com>
2024-12-16 12:41:58 -0700

    Added screen files

---
+++ lib/screens/profile_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class EventCreationScreen extends StatelessWidget {
+  const EventCreationScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(); // Add your screen content here
+  }
+}
