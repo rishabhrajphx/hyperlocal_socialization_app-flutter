01c4057e311873303d8bdf23b167c42b8d42cde6
rishabhrajphx <rishabhnewmob@gmail.com>
2024-12-16 13:18:27 -0700

    Fixed minor issues witht the even_card

--- lib/widgets/event_card.dart
+++ lib/widgets/event_card.dart
@@ -146,59 +146,6 @@ class _QuickActionButton extends StatelessWidget {
     );
   }
 }
-
-class EventDetailsScreen extends StatelessWidget {
-  final Event event;
-
-  const EventDetailsScreen({super.key, required this.event});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Event Details'),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.more_vert),
-            onPressed: () {
-            },
-          ),
-        ],
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    event.title,
-                    style: Theme.of(context).textTheme.headlineMedium,
-                  ),
-                  const SizedBox(height: 16),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
-      bottomNavigationBar: BottomAppBar(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: ElevatedButton(
-            onPressed: () {
-            },
-            child: const Text('Get Tickets'),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
 /* Future Improvements:
  * 1. Animations:
  *    - Add hero transitions for images
