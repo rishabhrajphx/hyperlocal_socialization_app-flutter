f053ff46b8df69cab72b44ef746bc5d041118a00
rishabhrajphx <rishabhnewmob@gmail.com>
2024-12-18 11:26:34 -0700

    friend invitation and friend list

---
+++ lib/data/repositories/user_repository.dart
@@ -0,0 +1,9 @@
+class UserRepository {
+  Future<void> sendFriendInvite(String userId) async {
+    // Implement friend invitation
+  }
+  
+  Stream<List<User>> getFriendsList() {
+    // Get real-time friends list
+  }
+} 
